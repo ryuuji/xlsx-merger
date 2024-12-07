c3d85d1f2ec57394922b20eb93f5399a589c2eb5
Ryuuji Yoshimoto <ryuuji@calil.jp>
2024-12-07 14:23:54 +0900

    Python3.13に対応

--- merger.py
+++ merger.py
@@ -1,4 +1,5 @@
 import click
+import os
 from openpyxl import Workbook, load_workbook
 
 
@@ -10,13 +11,24 @@ def merge(files):
     wb = Workbook()
 
     for filename in files:
-        click.echo(filename)
-        wb_load = load_workbook(filename, keep_vba=True)
-        for ws in wb_load.worksheets:
-            ws2 = wb.create_sheet()
-            for row in ws:
-                for cell in row:
-                    ws2[cell.coordinate].value = cell.value
+        if os.path.isdir(filename):
+            for filename_ in os.listdir(filename):
+                if filename_.endswith('.xlsx'):
+                    click.echo(filename + "\\" + filename_)
+                    wb_load = load_workbook(filename + "\\" + filename_, keep_vba=True)
+                    for ws in wb_load.worksheets:
+                        ws2 = wb.create_sheet()
+                        for row in ws:
+                            for cell in row:
+                                ws2[cell.coordinate].value = cell.value
+        else:
+            click.echo(filename)
+            wb_load = load_workbook(filename, keep_vba=True)
+            for ws in wb_load.worksheets:
+                ws2 = wb.create_sheet()
+                for row in ws:
+                    for cell in row:
+                        ws2[cell.coordinate].value = cell.value
     wb.save('merged.xlsx')
 
 
